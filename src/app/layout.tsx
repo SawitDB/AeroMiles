55a95377954c291c73605468773e4b02d87d7255
RPR <rivaldy.rivly@gmail.com>
2026-05-14 16:53:04 +0700

    fix: add @types/pg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Providers } from './providers'
-import { Navbar } from '@/components/Navbar.tsx'
+import { Navbar } from '@/components/Navbar'
 
 export const metadata: Metadata = {
   title: 'AeroMiles',
